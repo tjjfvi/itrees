808394e2d37a9366754f64d4f9ca632d3e06a985
tjjfvi <t6@t6.fyi>
2023-11-18 08:46:14 -0500

    bump allocator

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![feature(thread_local)]
+#![feature(allocator_api)]
+#![feature(const_mut_refs)]
 #![feature(new_uninit)]
 
 mod net;
@@ -7,13 +10,49 @@ mod print;
 mod tree;
 mod utils;
 
+#[global_allocator]
+#[thread_local]
+static ALLOCATOR: BumpAlloc = BumpAlloc(UnsafeCell::new((None, 1 << 20)));
+
+#[derive(Default)]
+struct BumpAlloc(UnsafeCell<(Option<(*mut u8, usize)>, usize)>);
+
+const MAX_CHUNK: usize = 1 << 30;
+
+unsafe impl GlobalAlloc for BumpAlloc {
+  unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
+    let inner = unsafe { &mut *self.0.get() };
+    if let Some((ptr, len)) = &mut inner.0 {
+      let offset = ptr.align_offset(layout.align());
+      let needed = offset + layout.size();
+      if needed < *len {
+        let r = ptr.offset(offset as isize);
+        *ptr = ptr.offset(needed as isize);
+        *len -= needed;
+        return r;
+      }
+    }
+    let len = (inner.1 << 1).min(MAX_CHUNK).max(layout.size());
+    inner.1 = len;
+    let alloc = System.alloc(Layout::from_size_align(len, layout.align()).unwrap());
+    inner.0 = Some((alloc.offset(layout.size() as isize), len - layout.size()));
+    alloc
+  }
+
+  unsafe fn dealloc(&self, _: *mut u8, _: std::alloc::Layout) {}
+}
+
 pub use net::*;
 pub use node::*;
 pub use parse::*;
 pub use print::*;
 pub use tree::*;
 
-use std::fmt::Debug;
+use std::{
+  alloc::{GlobalAlloc, Layout, System},
+  cell::UnsafeCell,
+  fmt::Debug,
+};
 
 #[allow(unused)]
 fn main() {
